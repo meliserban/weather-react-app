a4de691a0c10a6e44f0e12e5584253cb11f9ccf6
Melis Erban <csmeliserban@gmail.com>
2024-02-02 19:56:52 +0300

    search input added

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import TopButton from "./components/TopButton";
+import Inputs from "./components/Inputs";
+import "./App.css";
+import UilReact from "@iconscout/react-unicons/icons/uil-react";
+
+function App() {
+  return (
+    <div
+      className="mx-auto max-w-7xl		 mt-4 py-5 px-32 rounded-2xl
+    bg-gradient-to-br from-cyan-500 to-blue-900 h-fit shadow-xl
+    shadow-gray-400"
+    >
+      <TopButton />
+      <Inputs />
+    </div>
+  );
+}
+
+export default App;
